16d68347b2172163c5b2bf074f96c1c389ca8fe9
s28208 <s28208@pjwstk.edu.pl>
2024-03-15 17:31:58 +0100

    commit2

---
+++ project2/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+static double GetAverage(int[] numbers)
+{
+    int sum = 0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+    return (double) sum / numbers.Length;
+}
+int[] numbers = {1, 2, 3, 4};
+double average = GetAverage(numbers);
+Console.WriteLine(average);
