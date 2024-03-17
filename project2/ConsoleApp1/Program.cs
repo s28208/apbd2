51757aff1913a18a7d7717a739864ea5816c7f22
s28208 <s28208@pjwstk.edu.pl>
2024-03-17 16:29:11 +0100

    commit3

--- project2/ConsoleApp1/Program.cs
+++ project2/ConsoleApp1/Program.cs
@@ -2,15 +2,3 @@
 
 Console.WriteLine("Hello, World!");
 
-static double GetAverage(int[] numbers)
-{
-    int sum = 0;
-    foreach (var number in numbers)
-    {
-        sum += number;
-    }
-    return (double) sum / numbers.Length;
-}
-int[] numbers = {1, 2, 3, 4};
-double average = GetAverage(numbers);
-Console.WriteLine(average);
