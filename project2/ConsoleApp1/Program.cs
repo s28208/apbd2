881e62e02695a4698428077063a5c14a50746eda
s28208 <s28208@pjwstk.edu.pl>
2024-03-17 16:57:23 +0100

    add max

--- project2/ConsoleApp1/Program.cs
+++ project2/ConsoleApp1/Program.cs
@@ -1,4 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-
+static int GetMax(int[] numbers)
+{
+    int max = numbers[0];
+    foreach (var number in numbers)
+    {
+        if (max < number)
+        {
+            max = number;
+        }
+    }
+    return max;
+}
+int[] numbers = {1, 4, 3, 1};
+double max = GetMax(numbers);
+Console.WriteLine(max);
